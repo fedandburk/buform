3faebb76555da661eb55e23335b45a56cbd62724
Denys Fiediaiev <fiediaiev@fedandburk.com>
2024-02-15 19:40:03 +0000

    Migrate FluentValidation package to .NET 7

--- Buform.FluentValidation/FluentValidationForm.cs
+++ Buform.FluentValidation/FluentValidationForm.cs
@@ -1,70 +1,66 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using FluentValidation;
 using FluentValidation.Results;
 
-namespace Buform
+namespace Buform;
+
+public sealed class FluentValidationForm<TTarget> : ValidatableForm
 {
-    public sealed class FluentValidationForm<TTarget> : ValidatableForm
+    private readonly TTarget _target;
+    private readonly IValidator<TTarget> _validator;
+
+    public FluentValidationForm(TTarget target, IValidator<TTarget> validator) : base(target!)
     {
-        private readonly TTarget _target;
-        private readonly IValidator<TTarget> _validator;
+        _target = target;
+        _validator = validator;
+    }
 
-        public FluentValidationForm(TTarget target, IValidator<TTarget> validator) : base(target!)
-        {
-            _target = target;
-            _validator = validator;
-        }
+    private static FormValidationError GetValidationError(
+        string propertyName,
+        IEnumerable<ValidationFailure> failures
+    )
+    {
+        return new FormValidationError(
+            propertyName,
+            string.Join(Environment.NewLine, failures.Select(failure => failure.ErrorMessage))
+        );
+    }
 
-        private static FormValidationError GetValidationError(
-            string propertyName,
-            IEnumerable<ValidationFailure> failures
-        )
-        {
-            return new FormValidationError(
-                propertyName,
-                string.Join(Environment.NewLine, failures.Select(failure => failure.ErrorMessage))
-            );
-        }
+    private static FormValidationError[] GetValidationErrors(ValidationResult result)
+    {
+        return result.Errors
+            .GroupBy(item => item.PropertyName)
+            .Select(item => GetValidationError(item.Key, item.ToArray()))
+            .ToArray();
+    }
 
-        private static FormValidationError[] GetValidationErrors(ValidationResult result)
-        {
-            return result.Errors
-                .GroupBy(item => item.PropertyName)
-                .Select(item => GetValidationError(item.Key, item.ToArray()))
-                .ToArray();
-        }
+    public override bool PerformValidation()
+    {
+        var result = _validator.Validate(_target);
 
-        public override bool PerformValidation()
-        {
-            var result = _validator.Validate(_target);
+        IsValid = result.IsValid;
 
-            IsValid = result.IsValid;
+        SetValidationErrors(false, GetValidationErrors(result));
 
-            SetValidationErrors(false, GetValidationErrors(result));
+        return IsValid;
+    }
 
-            return IsValid;
-        }
+    public override bool PerformForcedValidation()
+    {
+        var result = _validator.Validate(_target);
 
-        public override bool PerformForcedValidation()
-        {
-            var result = _validator.Validate(_target);
+        IsValid = result.IsValid;
 
-            IsValid = result.IsValid;
+        SetValidationErrors(true, GetValidationErrors(result));
 
-            SetValidationErrors(true, GetValidationErrors(result));
+        return IsValid;
+    }
 
-            return IsValid;
-        }
+    public override void ResetValidation()
+    {
+        var result = _validator.Validate(_target);
 
-        public override void ResetValidation()
-        {
-            var result = _validator.Validate(_target);
+        IsValid = result.IsValid;
 
-            IsValid = result.IsValid;
-
-            ResetValidationErrors();
-        }
+        ResetValidationErrors();
     }
 }
