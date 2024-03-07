5424b0544cc387709a1a3050239bf6689f485df7
Denys Fiediaiev <fiediaiev@fedandburk.com>
2024-03-07 21:08:00 +0000

    Implement button component for Android

---
+++ Buform/Platforms/Android/Components/Button/ButtonFormComponent.cs
@@ -0,0 +1,14 @@
+using Android.Runtime;
+
+namespace Buform;
+
+[Preserve(AllMembers = true)]
+[FormComponent]
+// ReSharper disable once UnusedType.Global
+public sealed class ButtonFormComponent : IFormComponent
+{
+    public void Register()
+    {
+        FormPlatform.RegisterItem<ButtonFormItem, ButtonFormViewHolder>(Resource.Layout.FormItemButtonLayout);
+    }
+}
---
+++ Buform/Platforms/Android/Components/Button/ButtonFormViewHolder.cs
@@ -0,0 +1,103 @@
+using Android.Runtime;
+using Android.Views;
+using Fedandburk.Common.Extensions;
+
+namespace Buform;
+
+[Preserve(AllMembers = true)]
+public class ButtonFormViewHolder : FormViewHolder<ButtonFormItem>
+{
+    private Button? _button;
+
+    public ButtonFormViewHolder(IntPtr javaReference, JniHandleOwnership transfer) : base(javaReference, transfer)
+    {
+        /* Required constructor */
+    }
+
+    public ButtonFormViewHolder(View itemView) : base(itemView)
+    {
+        /* Required constructor */
+    }
+
+    private void OnButtonClick(object? sender, EventArgs e)
+    {
+        Item?.Value.SafeExecute();
+    }
+
+    protected override void Initialize()
+    {
+        _button = ItemView.FindViewById<Button>(Resource.Id.Button)!;
+        _button.Click += OnButtonClick;
+    }
+
+    protected virtual void UpdateReadOnlyState()
+    {
+        if (_button == null)
+        {
+            return;
+        }
+
+        var isReadOnly = Item?.IsReadOnly ?? true;
+
+        _button.Enabled = !isReadOnly;
+    }
+
+    protected virtual void UpdateLabel()
+    {
+        if (_button == null)
+        {
+            return;
+        }
+
+        _button.Text = Item?.Label;
+    }
+
+    protected virtual void UpdateInputType()
+    {
+        if (_button == null)
+        {
+            return;
+        }
+
+        // TODO: Update button appearance.
+    }
+
+    protected override void OnItemSet()
+    {
+        UpdateReadOnlyState();
+        UpdateLabel();
+        UpdateInputType();
+    }
+
+    protected override void OnItemPropertyChanged(string? propertyName)
+    {
+        switch (propertyName)
+        {
+            case nameof(Item.IsReadOnly):
+                UpdateReadOnlyState();
+                break;
+            case nameof(Item.Label):
+                UpdateLabel();
+                break;
+            case nameof(Item.InputType):
+                UpdateInputType();
+                break;
+        }
+    }
+
+    protected override void Dispose(bool disposing)
+    {
+        if (disposing)
+        {
+            var button = _button;
+            if (button != null)
+            {
+                button.Click -= OnButtonClick;
+            }
+
+            _button = null;
+        }
+
+        base.Dispose(disposing);
+    }
+}
