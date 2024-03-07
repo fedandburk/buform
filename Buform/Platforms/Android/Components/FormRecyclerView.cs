a4e27dd88087b042fcc4388eeb887035a684ccd1
Denys Fiediaiev <fiediaiev@fedandburk.com>
2024-03-07 21:06:07 +0000

    Add form recycler view

---
+++ Buform/Platforms/Android/Components/FormRecyclerView.cs
@@ -0,0 +1,60 @@
+using Android.Content;
+using Android.Runtime;
+using Android.Util;
+using AndroidX.RecyclerView.Widget;
+
+namespace Buform;
+
+[Preserve(AllMembers = true)]
+[Register("Buform.FormRecyclerView")]
+public class FormRecyclerView : RecyclerView
+{
+    public Form? Form
+    {
+        get => (GetAdapter() as FormRecyclerViewAdapter)!.Form;
+        set => (GetAdapter() as FormRecyclerViewAdapter)!.Form = value;
+    }
+
+    public override bool HasFixedSize => false;
+
+    protected FormRecyclerView(IntPtr javaReference, JniHandleOwnership transfer) : base(javaReference, transfer)
+    {
+        /* Required constructor */
+    }
+
+    public FormRecyclerView(Context context) : base(context)
+    {
+        Initialize();
+    }
+
+    public FormRecyclerView(Context context, IAttributeSet? attrs) : base(context, attrs)
+    {
+        Initialize();
+    }
+
+    public FormRecyclerView(Context context, IAttributeSet? attrs, int defStyleAttr) : base(context, attrs, defStyleAttr)
+    {
+        Initialize();
+    }
+
+    private void Initialize()
+    {
+        SetLayoutManager(CreateLayoutManager());
+        SetAdapter(CreateAdapter());
+    }
+
+    protected virtual LayoutManager CreateLayoutManager()
+    {
+        // TODO: Add support for multiple columns.
+        var layoutManager = new GridLayoutManager(Context, 1);
+
+        layoutManager.SetSpanSizeLookup(new GridLayoutManager.DefaultSpanSizeLookup());
+
+        return layoutManager;
+    }
+
+    protected virtual Adapter CreateAdapter()
+    {
+        return new FormRecyclerViewAdapter();
+    }
+}
