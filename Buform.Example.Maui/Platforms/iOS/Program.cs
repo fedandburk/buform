b1d6706bd4a43758a503e77c91c257b7497b4916
Denys Fiediaiev <fiediaiev@fedandburk.com>
2024-06-11 11:22:43 +0100

    Update Maui example

---
+++ Buform.Example.Maui/Platforms/iOS/Program.cs
@@ -0,0 +1,11 @@
+﻿using UIKit;
+
+namespace Buform;
+
+public class Program
+{
+    private static void Main(string[] args)
+    {
+        UIApplication.Main(args, null, typeof(AppDelegate));
+    }
+}
