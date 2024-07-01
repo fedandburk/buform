6cbd83f98d18e6c68c154bf08e2fb99d0e479409
Taraban Vladyslav <taraban@fedandburk.com>
2024-07-01 12:40:25 +0300

    Add code to reproduce exception

--- Buform.Example/ComponentsViewModel.cs
+++ Buform.Example/ComponentsViewModel.cs
@@ -25,6 +25,9 @@ public partial class ComponentsViewModel : ObservableObject
     private ObservableCollection<int> _list = new(Enumerable.Range(1, 10));
 
     [ObservableProperty]
+    private ObservableCollection<int> _list2 = new(Enumerable.Range(1, 10));
+
+    [ObservableProperty]
     private Enum _segments;
 
     [ObservableProperty]
@@ -80,239 +83,32 @@ public partial class ComponentsViewModel : ObservableObject
                 MoveCommand = MoveListItemCommand,
                 SelectCommand = SelectListItemCommand
             },
-            new TextFormGroup("Pickers")
+            new ListFormGroup<int, TextFormItem<int>>(
+                item => new TextFormItem<int>(item) { Formatter = i => i.ToWords() },
+                "List"
+            )
             {
-                new MultiValuePickerFormItem<int>(() => MultiValuePicker)
-                {
-                    Label = "Multi-value Picker",
-                    Message = "Please select a number.",
-                    CanBeCleared = true,
-                    InputType = PickerInputType.Default,
-                    ItemFormatter = item => item.ToWords(),
-                    OptionsFilterValueFactory = option => option.ToWords(),
-                    ValueFormatter = value => value?.Humanize() ?? "None",
-                    Source = Enumerable.Range(1, 10).ToArray()
-                },
-                new MultiValuePickerFormItem<int>(() => MultiValuePicker)
-                {
-                    Label = "Dialog Multi-value Picker",
-                    Message = "Please select a roman number.",
-                    CanBeCleared = true,
-                    InputType = PickerInputType.Dialog,
-                    ItemFormatter = item => item.ToRoman(),
-                    OptionsFilterValueFactory = option => option.ToRoman(),
-                    ValueFormatter = value => value?.Humanize() ?? "None",
-                    Source = Enumerable.Range(1, 10).ToArray()
-                },
-                new CallbackPickerFormItem(() => CallbackPicker)
-                {
-                    Label = "Callback Picker",
-                    Formatter = item => item ?? "None",
-                    Callback = () => Task.FromResult(new LipsumGenerator().GenerateWords(1)[0])!
-                },
-                new PickerFormItem<int?>(() => Picker)
-                {
-                    Label = "Picker",
-                    Message = "Please select a number.",
-                    CanBeCleared = true,
-                    InputType = PickerInputType.Default,
-                    Formatter = item => item?.ToWords() ?? "None",
-                    OptionsFilterValueFactory = option => option?.ToWords() ?? "None",
-                    Source = Enumerable.Range(1, 10).OfType<int?>().ToArray()
-                },
-                new PickerFormItem<int?>(() => Picker)
-                {
-                    Label = "Dialog Picker",
-                    Message = "Please select a metric number.",
-                    CanBeCleared = true,
-                    InputType = PickerInputType.Dialog,
-                    Formatter = item => item?.ToMetric() ?? "None",
-                    OptionsFilterValueFactory = option => option?.ToMetric() ?? "None",
-                    Source = Enumerable.Range(1, 10).OfType<int?>().ToArray()
-                },
-                new PickerFormItem<int?>(() => Picker)
-                {
-                    Label = "Pop-up Picker",
-                    Message = "Please select a roman number.",
-                    CanBeCleared = true,
-                    InputType = PickerInputType.PopUp,
-                    Formatter = item => item?.ToRoman() ?? "None",
-                    Source = Enumerable.Range(1, 10).OfType<int?>().ToArray()
-                },
-                new AsyncPickerFormItem<int?>(() => AsyncPicker)
-                {
-                    Label = "Async Picker",
-                    Message = "Please select a number.",
-                    CanBeCleared = true,
-                    InputType = PickerInputType.Default,
-                    Formatter = item => item?.ToWords() ?? "None",
-                    OptionsFilterValueFactory = option => option?.ToWords(),
-                    SourceFactory = async cancellationToken =>
-                    {
-                        await Task.Delay(TimeSpan.FromSeconds(3), cancellationToken)
-                            .ConfigureAwait(false);
-                        return Enumerable.Range(1, 10).OfType<int?>().ToArray();
-                    }
-                },
-                new AsyncPickerFormItem<int?>(() => AsyncPicker)
-                {
-                    Label = "Dialog Async Picker",
-                    Message = "Please select a roman number.",
-                    CanBeCleared = true,
-                    InputType = PickerInputType.Dialog,
-                    Formatter = item => item?.ToRoman() ?? "None",
-                    OptionsFilterValueFactory = option => option?.ToRoman() ?? "None",
-                    SourceFactory = async cancellationToken =>
-                    {
-                        await Task.Delay(TimeSpan.FromSeconds(3), cancellationToken)
-                            .ConfigureAwait(false);
-                        return Enumerable.Range(1, 10).OfType<int?>().ToArray();
-                    }
-                },
-                new ColorPickerFormItem(() => Color) { Label = "Color Picker" }
-            },
-            new TextFormGroup("Buttons")
-            {
-                new ButtonFormItem(WriteLineCommand)
-                {
-                    Label = "Default Button",
-                    InputType = ButtonInputType.Default
-                },
-                new ButtonFormItem(WriteLineCommand)
-                {
-                    Label = "Destructive Button",
-                    InputType = ButtonInputType.Destructive
-                },
-                new ButtonFormItem(WriteLineCommand)
-                {
-                    Label = "Done Button",
-                    InputType = ButtonInputType.Done
-                }
-            },
-            new TextFormGroup("Segments")
-            {
-                new SegmentsFormItem<Enum>(() => Segments)
-                {
-                    Label = "Segments",
-                    Source = System.Enum.GetValues(typeof(Enum)).OfType<Enum>()
-                }
-            },
-            new TextFormGroup("Texts")
-            {
-                new TextInputFormItem(() => Text)
-                {
-                    Placeholder = "Default text",
-                    InputType = TextInputType.Default
-                },
-                new TextInputFormItem(() => Text)
-                {
-                    Label = "Number & Punctuation Text",
-                    InputType = TextInputType.NumberAndPunctuation
-                },
-                new TextInputFormItem(() => Text)
-                {
-                    Label = "Number Text",
-                    InputType = TextInputType.Number
-                },
-                new TextInputFormItem(() => Text)
-                {
-                    Label = "Decimal Text",
-                    InputType = TextInputType.Decimal
-                },
-                new TextInputFormItem(() => Text)
-                {
-                    Label = "Phone Text",
-                    InputType = TextInputType.Phone
-                },
-                new TextInputFormItem(() => Text)
-                {
-                    Label = "Url Text",
-                    InputType = TextInputType.Url
-                },
-                new TextInputFormItem(() => Text)
-                {
-                    Label = "Email Address Text",
-                    InputType = TextInputType.EmailAddress
-                },
-                new TextInputFormItem(() => Text) { Label = "Password Text", IsSecured = true },
-                new MultilineTextInputFormItem(() => MultilineText)
-                {
-                    Placeholder = "Multiline text"
-                }
-            },
-            new TextFormGroup("Sliders")
-            {
-                new TextInputFormItem<float>(
-                    () => Slider,
-                    @string => float.TryParse(@string, out var value) ? value : 0
-                )
-                {
-                    Label = "Slider value",
-                    InputType = TextInputType.Decimal
-                },
-                new SliderFormItem(() => Slider) { MinValue = 0, MaxValue = 10 }
-            },
-            new TextFormGroup("Switches")
-            {
-                new SwitchFormItem(() => Switch)
-                {
-                    Label = "Switch",
-                    ValueChangedCallback = (form, value) =>
-                        form.GetItem(() => HiddenText)!.IsVisible = value
-                },
-                new TextInputFormItem(() => HiddenText) { Label = "Hidden text", IsVisible = false }
-            },
-            new TextFormGroup("Steppers")
-            {
-                new TextInputFormItem<int>(
-                    () => Stepper,
-                    @string => int.TryParse(@string, out var value) ? value : 0
-                )
-                {
-                    Label = "Stepper value",
-                    InputType = TextInputType.Number
-                },
-                new StepperFormItem(() => Stepper)
-                {
-                    Label = "Stepper",
-                    StepAmount = 5,
-                    MinValue = -10,
-                    MaxValue = 10
-                }
-            },
-            new TextFormGroup("Date & Time")
-            {
-                new DateTimeFormItem(() => DateTime)
-                {
-                    Label = "Date and time",
-                    InputType = DateTimeInputType.DateTime
-                },
-                new DateTimeFormItem(() => DateTime)
-                {
-                    Label = "Date",
-                    InputType = DateTimeInputType.Date
-                },
-                new DateTimeFormItem(() => DateTime)
-                {
-                    Label = "Time",
-                    InputType = DateTimeInputType.Time
-                }
-            },
-            new TextFormGroup("Custom views & items", "Demonstrates custom items and item views")
-            {
-                new RandomNumberGeneratorItem(() => RandomNumber)
-            },
-            new TextFormGroup("MvvmCross", "Demonstrates MvvmCross bindings in item views")
-            {
-                // new MvxButtonFormItem(WriteLineCommand) { Label = "MvvmCross Button" }
-            }
+            Source = List2,
+            RemoveCommand = RemoveListItemCommand,
+            MoveCommand = MoveListItemCommand,
+            SelectCommand = SelectListItemCommand
+        }                                        
         };
     }
 
     [RelayCommand]
     private void ToggleReadOnlyMode()
     {
-        Form.IsReadOnly = !Form.IsReadOnly;
+        if (List2.Count > 3)
+        {
+            List.Add(1);
+            List2.RemoveAt(2);
+        }
+        else
+        {
+            List2.Add(1);
+            List.RemoveAt(2);
+        }
     }
 
     [RelayCommand]
