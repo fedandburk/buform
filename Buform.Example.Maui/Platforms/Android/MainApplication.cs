0d7757370e31afbaec15bd829ea43cc4767c655f
Evgen Brovchenko <brovchenko@fedandburk.com>
2026-03-10 15:51:26 +0200

    Clean up

--- Buform.Example.Maui/Platforms/Android/MainApplication.cs
+++ Buform.Example.Maui/Platforms/Android/MainApplication.cs
@@ -8,9 +8,9 @@ public class MainApplication : MauiApplication
 {
     public MainApplication(IntPtr handle, JniHandleOwnership ownership)
         : base(handle, ownership) { }
-
+    
     protected override MauiApp CreateMauiApp()
     {
-        return  MauiProgram.CreateMauiApp();
+        return MauiProgram.CreateMauiApp();
     }
 }
