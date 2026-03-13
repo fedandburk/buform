9da84765e0a1594aed68b24593085513d80b05bd
Evgen Brovchenko <brovchenko@fedandburk.com>
2026-03-13 16:07:40 +0200

    Clean up

--- Buform.Maui/Platforms/Android/MauiFormItemViewHolder.cs
+++ Buform.Maui/Platforms/Android/MauiFormItemViewHolder.cs
@@ -50,15 +50,13 @@ internal sealed class MauiFormItemViewHolder : MauiFormViewHolderBase<FormItemVi
         _fallbackTitleView.Text = title;
         _fallbackButtonItem = item as ButtonFormItem;
 
-        //Console.WriteLine(" +++++++++++++ _fallbackButtonItem: "+ (_fallbackButtonItem == null));
-
         myItem = item;
-            
+
         ShowFallback(_fallbackRoot);
     }
 
     public object myItem;
-    
+
     private void EnsureFallbackView(Context context)
     {
         if (_fallbackRoot != null)
@@ -102,16 +100,9 @@ internal sealed class MauiFormItemViewHolder : MauiFormViewHolderBase<FormItemVi
 
     private void OnFallbackRootClick(object? sender, EventArgs e)
     {
-         
-        
-        Console.WriteLine(" +++++++++++++ myItem : "+myItem.GetType());
         _fallbackButtonItem?.Value.SafeExecute();
 
         var tmp = myItem as MultiValuePickerFormItem;
-        if (tmp != null)
-        {
-            Console.WriteLine(" +++++++++++++ nice!!!!");
-            tmp.Pick(null);
-        }
+        tmp?.Pick(null);
     }
 }
