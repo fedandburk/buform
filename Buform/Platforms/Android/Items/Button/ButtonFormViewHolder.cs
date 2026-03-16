65779ac69a200c821515316d74dfd4fb7a12fb71
Evgen Brovchenko <brovchenko@fedandburk.com>
2026-03-16 19:54:40 +0200

    Redesign ButtonFormViewHolder for MD

--- Buform/Platforms/Android/Items/Button/ButtonFormViewHolder.cs
+++ Buform/Platforms/Android/Items/Button/ButtonFormViewHolder.cs
@@ -1,13 +1,18 @@
+using Android.Content.Res;
+using Android.Graphics;
 using Android.Runtime;
+using Android.Util;
 using Android.Views;
 using Fedandburk.Common.Extensions;
+using Google.Android.Material.Button;
+using Google.Android.Material.Color;
 
 namespace Buform;
 
 [Preserve(AllMembers = true)]
 public class ButtonFormViewHolder : FormViewHolder<ButtonFormItem>
 {
-    private Button? _button;
+    private MaterialButton? _button;
 
     public ButtonFormViewHolder(IntPtr javaReference, JniHandleOwnership transfer)
         : base(javaReference, transfer)
@@ -28,8 +33,18 @@ public class ButtonFormViewHolder : FormViewHolder<ButtonFormItem>
 
     protected override void Initialize()
     {
-        _button = ItemView.FindViewById<Button>(Resource.Id.Button)!;
+        _button = ItemView.FindViewById<MaterialButton>(
+            _Microsoft.Android.Resource.Designer.Resource.Id.Button
+        )!;
         _button.Click += OnButtonClick;
+
+        _button.SetAllCaps(false);
+        _button.IconGravity = MaterialButton.IconGravityTextStart;
+        _button.InsetTop = 0;
+        _button.InsetBottom = 0;
+        _button.CornerRadius = Dp(20);
+        _button.StrokeWidth = 0;
+        _button.Elevation = 0;
     }
 
     protected virtual void UpdateReadOnlyState()
@@ -40,7 +55,6 @@ public class ButtonFormViewHolder : FormViewHolder<ButtonFormItem>
         }
 
         var isReadOnly = Data?.IsReadOnly ?? true;
-
         _button.Enabled = !isReadOnly;
     }
 
@@ -61,7 +75,35 @@ public class ButtonFormViewHolder : FormViewHolder<ButtonFormItem>
             return;
         }
 
-        // TODO: Update button appearance.
+        var button = _button;
+
+        var colorPrimary = GetThemeColor(
+            button,
+            _Microsoft.Android.Resource.Designer.Resource.Attribute.colorPrimary
+        );
+
+        var ripple = CreateColorStateList(ApplyAlpha(colorPrimary, 0.12f));
+
+        button.StrokeWidth = 0;
+        button.StrokeColor = null;
+        button.BackgroundTintList = null;
+        button.SetTextColor(colorPrimary);
+        button.RippleColor = ripple;
+        button.Elevation = 0;
+        button.StateListAnimator = null;
+
+        ApplyTextStyle(button, colorPrimary, ripple);
+    }
+
+    private void ApplyTextStyle(MaterialButton button, Color textColor, ColorStateList ripple)
+    {
+        button.BackgroundTintList = CreateColorStateList(Color.Transparent);
+        button.SetTextColor(textColor);
+        button.RippleColor = ripple;
+        button.StrokeWidth = 0;
+        button.StrokeColor = null;
+        button.Elevation = 0;
+        button.TranslationZ = 0;
     }
 
     protected override void OnDataSet()
@@ -78,9 +120,11 @@ public class ButtonFormViewHolder : FormViewHolder<ButtonFormItem>
             case nameof(Data.IsReadOnly):
                 UpdateReadOnlyState();
                 break;
+
             case nameof(Data.Label):
                 UpdateLabel();
                 break;
+
             case nameof(Data.InputType):
                 UpdateInputType();
                 break;
@@ -101,5 +145,31 @@ public class ButtonFormViewHolder : FormViewHolder<ButtonFormItem>
         }
 
         base.Dispose(disposing);
+    }
+
+    private int Dp(int value)
+    {
+        return (int)
+            TypedValue.ApplyDimension(
+                ComplexUnitType.Dip,
+                value,
+                ItemView.Context?.Resources?.DisplayMetrics
+            );
+    }
+
+    private static ColorStateList CreateColorStateList(Color color)
+    {
+        return ColorStateList.ValueOf(color);
+    }
+
+    private static Color GetThemeColor(View view, int attr)
+    {
+        return new Color(MaterialColors.GetColor(view, attr, Color.Magenta));
+    }
+
+    private static Color ApplyAlpha(Color color, float alpha)
+    {
+        var a = (int)(255 * alpha);
+        return Color.Argb(a, color.R, color.G, color.B);
     }
 }
