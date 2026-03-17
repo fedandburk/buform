bc518c2a11751bd8168733fd4f89aa47fe590ca0
Evgen Brovchenko <brovchenko@fedandburk.com>
2026-03-17 16:26:28 +0200

    Format text

--- Buform/Platforms/Android/Items/Text/TextFormViewHolder.cs
+++ Buform/Platforms/Android/Items/Text/TextFormViewHolder.cs
@@ -49,7 +49,7 @@ public class TextFormViewHolder : FormViewHolder<ITextFormItem>
         if (_textView == null)
             return;
 
-        _textView.Text = Data?.FormattedValue ?? string.Empty;
+        _textView.Text = Data?.FormattedValue ?? Data?.Value?.ToString() ?? string.Empty;
     }
 
     protected override void OnDataSet()
