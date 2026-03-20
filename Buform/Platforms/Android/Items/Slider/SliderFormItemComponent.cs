6ac485023102741221f555e6f09cef45ccf65b6c
Evgen Brovchenko <brovchenko@fedandburk.com>
2026-03-20 18:57:32 +0200

    Format codebase

--- Buform/Platforms/Android/Items/Slider/SliderFormItemComponent.cs
+++ Buform/Platforms/Android/Items/Slider/SliderFormItemComponent.cs
@@ -5,7 +5,7 @@ namespace Buform;
 [Preserve(AllMembers = true)]
 [FormComponent]
 // ReSharper disable once UnusedType.Global
-public sealed class SliderFormItemComponent: IFormComponent
+public sealed class SliderFormItemComponent : IFormComponent
 {
     public void Register()
     {
